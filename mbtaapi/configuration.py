2118b5026ef63af947195e2ac5a334c7513e9e90
Nick Bagley <nmbags@gmail.com>
2018-07-17 20:48:28 -0400

    fixing config to point to the actual API

--- mbtaapi/configuration.py
+++ mbtaapi/configuration.py
@@ -45,7 +45,7 @@ class Configuration(object):
         Constructor
         """
         # Default Base url
-        self.host = "https://localhost"
+        self.host = "https://api-v3.mbta.com/"
         # Default api client
         self.api_client = None
         # Temp file folder for downloading files
